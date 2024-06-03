154230b42b76a535712b94b55b234674676ca9bf
Stanisław Jarocki <s.jarocki354@gmail.com>
2024-06-03 21:07:52 +0200

    Replace values & lots more

---
+++ README.md
@@ -0,0 +1,7 @@
+the filled docx files will be named without national diatrict
+installation of libre office packet is required
+in env you can specify your own values
+values can be edited by passing them on startup
+-t for checking the data, no executing app
+-d for keeping the docx, not only pdf
+-h for help
--- index.js
+++ index.js
@@ -1,56 +1,158 @@
-require('dotenv').config();
 const fs = require('fs');
-
+const path = require('path');
+const PizZip = require("pizzip");
+const Docxtemplater = require("docxtemplater");
+const readlineSync = require('readline-sync');
 const libre = require('libreoffice-convert');
+const removeDiacritics = require('diacritics').remove;
 libre.convertAsync = require('util').promisify(libre.convert);
 
-const date = new Date();
-const currYear = date.getFullYear();
-const currMonthIdx = date.getMonth();
-const prevMonthNum = currMonthIdx === 0 ? 12 : currMonthIdx;
-const targetYear = prevMonthNum === 12 ? currYear - 1 : currYear;
+const getAllValues = () => {
+    const date = new Date();
+    const currYear = date.getFullYear();
+    const currMonthIdx = date.getMonth();
+    const prevMonthNum = currMonthIdx === 0 ? 12 : currMonthIdx;
+    const targetYear = prevMonthNum === 12 ? currYear - 1 : currYear;
+    const targetMonth = prevMonthNum < 10 ? '0' + prevMonthNum : prevMonthNum;
+    const currMonthFirstDay = new Date(date.getFullYear(), date.getMonth(), 1);
+    const prevMonthLastDay = new Date(currMonthFirstDay - 1).getDate();
+    const targetDay = prevMonthLastDay.toString()
+    const currSchoolYear = currMonthIdx >= 8 ? `${currYear}/${currYear + 1}` : `${currYear - 1}/${currYear}`;
 
-const targetMonth = prevMonthNum < 10 ? '0' + prevMonthNum : prevMonthNum;
-const currSchoolYear = currMonthIdx >= 8 ? `${currYear}/${currYear + 1}` : `${currYear - 1}/${currYear}`;
-
-const inputPaths = process.env.INPUT.split(',');
-const args = process.argv.slice(2);
-
-const defaultValues = {
-    workedHours: process.env.WORKED_HOURS,
-    targetMonth: targetMonth,
-    targetYear: targetYear,
-    rate: process.env.RATE,
-    graduationDate: process.env.GRADUATION_DATE,
-    university: process.env.UNIVERSITY,
-    faculty: process.env.FACULTY,
-    address: process.env.ADDRESS,
-    name: process.env.NAME,
-    schoolYear: currSchoolYear
-};
-
-const values = args.reduce((acc, arg) => {
-    const [key, value] = arg.split('=');
-    return {
-        ...acc,
-        [key]: value
+    const defaultValues = {
+        targetDate: `${targetYear}-${targetMonth}-${targetDay}`,
+        targetYear: targetYear.toString(),
+        targetMonth: targetMonth,
+        targetDay: targetDay,
+        schoolYear: currSchoolYear,
+        ...getEnvValues(),
+        ...getArgsValues()
     };
-}, defaultValues);
 
-const [firstName, lastName] = values.name.split(' ')
+    return defaultValues;
+}
 
-console.log(`Files ${inputPaths} will be copied and filled with data: `, replacements);
-console.log('You can override any value with argument in format: fieldName=value, for example: ');
-console.log('node index.js targetMonth=04');
 
-for (inputPathIdx in inputPaths) {
-    const inputPath = inputPaths[inputPathIdx];
-    const originFilaName = inputPath.match (/.*_(.*)\.docx/)[1];
-    const outputPath = `./${targetYear}_${targetMonth}_${originFilaName}_${firstName}_${lastName}.pdf`;
 
-    convertToPdf(inputPath, outputPath)
-    .catch(function (err) {
-        console.log(`Error while converting file: ${err}`);
+
+const getCamelCase = (text, separator) => {
+    if (!text.includes(separator)) return text.toLowerCase();
+    const words = text.split(separator).map(word => word.toLowerCase());
+    const camelCase = words.reduce((acc, word, idx) => {
+        return acc + (
+            idx === 0
+                ? word
+                : word.charAt(0).toUpperCase() + word.slice(1)
+        );
+    }, '');
+    return camelCase;
+}
+
+const getEnvValues = () => {
+    const env = fs.readFileSync(path.resolve('.env'), 'utf8').split('\n')
+    const envValues = env.reduce((acc, line, idx) => {
+        const [key, value] = line.split('=')
+        const formattedKey = getCamelCase(key, '_');
+        const rawValue = value.replace(/['"]/g, '');
+
+        return {
+            ...acc,
+            [formattedKey]: rawValue
+        };
+    }, {});
+
+    return envValues;
+}
+
+const getArgsValues = (values) => {
+    const args = process.argv.slice(2);
+
+    args.reduce((acc, arg) => {
+        const [key, value] = arg.split('=');
+        return {
+            ...acc,
+            [key]: value
+        };
+    }, values);
+}
+
+const getConfirmation = (inputPaths, values) => {
+    console.log(`Files ${inputPaths} will be copied and filled with data: `, values);
+
+    process.stdout.write('');
+    const userInput = readlineSync.question('Do you want to proceed? (Y/n): ');
+    if (userInput === '' || userInput === 'y' || userInput === 'Y') {
+        console.log('Proceeding...')
+        return true;
+    } else if (userInput === 'n') {
+        console.log('Aborting...')
+        console.log("⚙️ You can set your own values in .env file:");
+        console.log("\tUse the following format: FIELD_NAME='value'");
+        console.log("\tFor example: TARGET_MONTH='04'");
+        console.log('⚙️ You can override any value with argument:');
+        console.log("\tUse the following format: fieldName=value");
+        console.log("\tFor example: targetMonth=04");
+        return false;
+    }
+
+    process.stdout.write('Please type "y" for yes, confirm with Enter or or "n" for no: ');
+    getConfirmation(inputPaths, values);
+}
+
+const getFilledDocs = (inputPaths, values) => {
+    const filledDocs = inputPaths.map((inputPath) => {
+        const { targetYear, targetMonth, firstName, lastName } = values;
+        const originFilaName = inputPath.match(/.*_(.*)\.docx/)[1];
+        const outputPath = `${targetYear}_${targetMonth}_${originFilaName}_${firstName}_${lastName}`;
+
+        const content = fs.readFileSync(
+            path.resolve(inputPath),
+            "binary"
+        );
+
+        const zip = new PizZip(content);
+
+        const doc = new Docxtemplater(zip, {
+            paragraphLoop: true,
+            linebreaks: true,
+        });
+
+        doc.render(values);
+
+        return {
+            doc,
+            outputPath,
+        };
+    });
+
+    return filledDocs;
+}
+
+const saveFiles = (docs, targetType) => {
+    docs.forEach(({ doc, outputPath }) => {
+        const normalizedOutputPath = removeDiacritics(outputPath)
+
+        const buf = doc.getZip().generate({
+            type: "nodebuffer",
+            compression: "DEFLATE",
+        });
+
+        fs.writeFileSync(path.resolve(__dirname, `${normalizedOutputPath}.docx`), buf);
+
+        if (targetType === 'pdf') {
+            convertToPdf(normalizedOutputPath + '.docx', outputPath + '.pdf')
+                .catch(function (err) {
+                    console.log(`Error while converting file: ${err}`);
+                })
+                .then(() => {
+                    console.log('PDF saved: ', `${outputPath}.pdf`)
+                    fs.unlinkSync(path.resolve(__dirname, `${normalizedOutputPath}.docx`));
+                })
+        } else if (targetType === 'docx') {
+            console.log('DOCX saved: ', `${normalizedOutputPath}.docx`)
+        } else {
+            console.log("Unsupported file type. Supported types are: pdf, docx. Specyfiy them in .env file e.g. TARGET_TYPE='pdf'")
+        }
     });
 }
 
@@ -64,3 +166,21 @@ async function convertToPdf(inputPath, outputPath) {
     fs.writeFileSync(outputPath, pdfBuf);
 }
 
+const main = () => {
+    const values = getAllValues();
+    const { templatePaths } = values;
+    if (!templatePaths) {
+        console.log("Please provide templatePaths in .env file separated by comma e.g. TEMPLATE_PATHS='template_work.docx,template_student.docx");
+        return;
+    }
+    const inputPaths = templatePaths.split(',');
+    const shouldContinue = getConfirmation(inputPaths, values);
+
+    if (!shouldContinue) {
+        return;
+    }
+    const filledDocs = getFilledDocs(inputPaths, values);
+    saveFiles(filledDocs, values.targetType);
+}
+
+main();
